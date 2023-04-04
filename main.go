b953bc40edea1ff1969cab362445069ed8c5c388
phdevbr <pedroheringer2020@gmail.com>
2023-04-04 16:35:34 -0300

    added username support, and improved send command

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 )
 
@@ -35,6 +34,7 @@ func main() {
 	fmt.Println("Listening on " + host + ":" + port)
 
 	clients := []net.Conn{}
+	names := make(map[string]string)
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
@@ -43,11 +43,12 @@ func main() {
 		}
 		fmt.Println("New client connected:", conn.RemoteAddr())
 		clients = append(clients, conn)
-		go handleRequest(conn, &clients)
+		go handleRequest(conn, &clients, &names)
 	}
 }
 
-func handleRequest(conn net.Conn, clients *[]net.Conn) {
+func handleRequest(conn net.Conn, clients *[]net.Conn, names *map[string]string) {
+main:
 	for {
 		reader := bufio.NewReader(conn)
 		message, err := reader.ReadString('\n')
@@ -55,6 +56,11 @@ func handleRequest(conn net.Conn, clients *[]net.Conn) {
 			conn.Close()
 			fmt.Println("Client", conn.RemoteAddr(), "disconnected")
 			*clients = RemoveConn(*clients, conn)
+			for user, ip := range *names {
+				if ip == conn.RemoteAddr().String() {
+					delete(*names, user)
+				}
+			}
 			break
 		}
 		// == Treating the message ==
@@ -63,29 +69,69 @@ func handleRequest(conn net.Conn, clients *[]net.Conn) {
 		rR := strings.ReplaceAll(rT, "\r", "")
 		fUnquoted := strings.ReplaceAll(rR, "\b", "")
 		// ==========================
-		if fUnquoted == "show" {
-			clientsLength := strconv.Itoa(len(*clients))
-			clientsStr := fmt.Sprintf("%d\n", clients)
-			conn.Write([]byte("Found " + clientsLength + " clients: " + clientsStr))
-		} else if strings.HasPrefix(fUnquoted, "send") {
+		if strings.HasPrefix(fUnquoted, "/send") {
 			if len(*clients) > 1 {
 				send := strings.Fields(message)
 				if len(send) > 2 {
-					addr := send[1]
-					for _, c := range *clients {
-						if c.RemoteAddr().String() == addr {
-							message := strings.Join(send[2:], " ")
-							c.Write([]byte(conn.RemoteAddr().String() + " sent you a message: " + message + "\n"))
+					receiver := send[1]
+					var sender string
+					for senderName, senderIp := range *names {
+						if conn.RemoteAddr().String() == senderIp {
+							sender = senderName
 							break
+						}
+					}
+					if len(sender) == 0 {
+						conn.Write([]byte("You need to be registered to send a message\n"))
+						continue main
+					}
+					for name, ip := range *names {
+						if name == receiver {
+							message := strings.Join(send[2:], " ")
+							for _, c := range *clients {
+								if c.RemoteAddr().String() == ip {
+									c.Write([]byte(sender + " sent you a message: " + message + "\n"))
+									continue main
+								}
+							}
 						}
 					}
 					conn.Write([]byte("Client not found!\n"))
 				} else {
-					conn.Write([]byte("Wrong format! Please use: send 'addr(ip:port)'. Example: send 127.0.0.1:56120\n"))
+					conn.Write([]byte("Wrong format! Use: /send 'addr(ip:port)' message.\n"))
 				}
 			} else {
 				fmt.Println("not enough clients")
 			}
+		} else if strings.HasPrefix(fUnquoted, "/register") {
+			if len(strings.Fields(fUnquoted)) > 1 {
+				name := strings.Fields(fUnquoted)[1]
+				if _, ok := (*names)[name]; ok {
+					conn.Write([]byte("The username " + name + " is already in use\n"))
+				} else {
+					userIp := conn.RemoteAddr().String()
+					for username, ip := range *names {
+						if ip == userIp {
+							conn.Write([]byte("You are already registered with the " + username + " username.\n"))
+							continue main
+						}
+					}
+					(*names)[name] = userIp
+					conn.Write([]byte("Você foi registrado com sucesso!\n"))
+				}
+			}
+		} else if fUnquoted == "/users" {
+			var users []string
+			for key := range *names {
+				users = append(users, key)
+			}
+			if len(users) < 1 {
+				conn.Write([]byte("There aren't any users online\n"))
+				continue main
+			}
+			usersStringify := strings.Join(users, ",")
+			message := fmt.Sprintf("There are %d users online: %s\n", len(users), usersStringify)
+			conn.Write([]byte(message))
 		} else {
 			conn.Write([]byte("Command not found.\n"))
 		}
